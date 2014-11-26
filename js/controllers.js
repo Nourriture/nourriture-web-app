983b0a9cd3960725dd5677f911a7b1f3feeb0d06
Niels Søholm <nm@9la.dk>
2014-11-26 13:48:32 +0800

    Implemented basic Hello World example that can be used as the foundation for the application

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,12 @@
 /**
  * Created by niels on 11/26/14.
+ *
+ * Main angular script file where controllers are either defined or referenced
  */
+
+var app = angular.module('NourritureWebApp', []);
+
+app.controller("MainCtrl", ['$scope', '$http', function ($scope, $http) {
+    // Application state
+    $scope.name = "John";
+}]);
