7d070342f4c6ba1eb3e2ee77ada33305bc7a0a7d
Niels Søholm <nm@9la.dk>
2014-11-30 08:30:05 +0800

    Extended the angular-js foundation of our web app to include an example of loading ingredients from backend and displaying them in a simple list.

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,27 @@
  */
 
 var app = angular.module('NourritureWebApp', []);
+var host = "http://localhost:2121";
+//var host = "http://dev.9la.dk/platform";
 
 app.controller("MainCtrl", ['$scope', '$http', function ($scope, $http) {
     // Application state
     $scope.name = "World";
+    $scope.ingredients = [];
+    $scope.loaderror = false;
+
+    // Get ingredients
+    $http.get(host + "/ingredient")
+        .then(
+            // Success
+            function (res) {
+                $scope.loaderror = false;
+                $scope.ingredients = res.data;
+            },
+            // Failure
+            function (res) {
+                $scope.loaderror = true;
+                console.log("Failed to retrieve ingredients");
+            }
+        );
 }]);
